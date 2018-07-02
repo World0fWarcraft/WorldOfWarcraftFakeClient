c85d73d7bcf0911dba4146742dcd44cb41096f2a
Mihai <mihai.gabriel1608@gmail.com>
2018-07-02 05:54:17 -0700

    Update WS.Movement.vb

--- WS.Movement.vb
+++ WS.Movement.vb
@@ -1,5 +1,7 @@
 Public Module WS_Movement
     Public Sub On_SMSG_SET_REST_START(ByRef Packet As PacketClass)
         Console.WriteLine("[{0}][World] You're now inside the world.", Format(TimeOfDay, "HH:mm:ss"))
+        'that's it... 
+        'needs moving by packets and x-y-z positions in space controlable by console 
     End Sub
 End Module
