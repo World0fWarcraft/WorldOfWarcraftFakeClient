46be9f31d589c7b923086bb4416909449a34f17a
Mihai <mihai.gabriel1608@gmail.com>
2018-07-02 05:54:58 -0700

    still needs work done

--- WS.WardenClient.vb
+++ WS.WardenClient.vb
@@ -1,21 +1,3 @@
-﻿'
-' Copyright (C) 2013 - 2018 getMaNGOS <https://getmangos.eu>
-'
-' This program is free software; you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation; either version 2 of the License, or
-' (at your option) any later version.
-'
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-'
-' You should have received a copy of the GNU General Public License
-' along with this program; if not, write to the Free Software
-' Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-'
-
 Imports System.IO
 Imports Emil.GMP
 Imports System.Security.Cryptography
@@ -25,8 +7,8 @@ Public Module WS_WardenClient
 
 #Region "Consts"
     Public Enum MaievResponse As Byte
-        MAIEV_RESPONSE_FAILED_OR_MISSING = &H0          'The module was either currupt or not in the cache request transfer
-        MAIEV_RESPONSE_SUCCESS = &H1                    'The module was in the cache and loaded successfully
+        MAIEV_RESPONSE_FAILED_OR_MISSING = &H0          'the module was either currupt or not in the cache request transfer
+        MAIEV_RESPONSE_SUCCESS = &H1                    'the module was in the cache and loaded successfully
         MAIEV_RESPONSE_RESULT = &H2
         MAIEV_RESPONSE_HASH = &H4
     End Enum
